50ca42d7da45687428eb67f2880e3b8c384af996
bombik815 <bombik815@gmail.com>
2024-08-07 14:50:29 +0300

    add host an port parameters from config

--- app/main.py
+++ app/main.py
@@ -1,18 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
 
+from core.config import settings
+
+from api import router as api_router
+
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(
+    api_router,
+    prefix=settings.api.prefix,
+)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.api.host,
+        port=settings.api.port,
+        reload=True,
+    )
