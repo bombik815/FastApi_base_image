eef3061144770cc6db54a901279cc658806df756
bombik815 <bombik815@gmail.com>
2024-08-12 17:52:14 +0300

    add SettingsConfigDict, configure model_config from .env

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,)
 
 
 class RunConfig(BaseModel):
@@ -18,9 +20,16 @@ class DatabaseConfig(BaseModel):
     max_overflow: int = 10
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
 
 
 settings = Settings()
+print(settings.db.url)
