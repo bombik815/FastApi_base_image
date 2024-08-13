75d60f8b4d332bf4439d9feb66ae96c3ba540b2d
bombik815 <bombik815@gmail.com>
2024-08-13 13:59:02 +0300

    add naming convention for models

--- app/core/config.py
+++ app/core/config.py
@@ -14,29 +14,23 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
-# class DatabaseConfig(BaseModel):
-# url: PostgresDsn
-# echo: bool = False
-# echo_pool: bool = False
-# pool_size: int = 50
-# max_overflow: int = 10
-
-
 class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
-    # model_config = SettingsConfigDict(
-    #     env_file=(".env.template", ".env"),
-    #     case_sensitive=False,
-    #     env_nested_delimiter="__",
-    #     env_prefix="APP_CONFIG__",
-    # )
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
-    # db: DatabaseConfig
 
     @property
     def DATABASE_URL(self):
